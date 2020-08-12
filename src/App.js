1e95798df9e2be0f3a403e2467fe009d514f691b
Thomas Stussi <tstussi06@gmail.com>
2020-08-12 16:29:30 -0700

    remade repo cause last one was being difficult

--- src/App.js
+++ src/App.js
@@ -1,26 +1,37 @@
+/* eslint-disable */
 import React from 'react';
-import logo from './logo.svg';
+import { fetchFoods } from './foods-api.js';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  state = {
+    foods: [] 
+  }
+
+  componentDidMount = async () => {
+    const data = await fetchFoods()
+    
+    this.setState({
+      foods: data.body
+    })
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <h2>foods:</h2>
+          {
+            this.state.foods.map((food) => {
+              return <div style={{ margin: 5, padding: 5, border: 'solid 3px white'}}>
+                {food.name} : {food.deliciousness}
+              </div>
+            })
+          }
+        </header>
+        </div>
+    )
+}
 }
 
 export default App;
